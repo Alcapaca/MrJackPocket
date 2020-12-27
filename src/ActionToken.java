f5dca35a9f35a32f10c9b7b9838e32e77dd478c9
nepton28 <nono.herrmann@gmail.com>
2020-12-27 15:34:46 +0100

    Commit-19/12

--- src/ActionToken.java
+++ src/ActionToken.java
@@ -1,4 +1,17 @@
 
 public class ActionToken {
-
+	private String name;
+	private int face;
+	
+	
+	public CardToken(String name, int face) {
+		
+	this.name = name;
+	this.face = face;
+	}	
+	
+	public String toString() {
+        return name + " " + face;
+    }
+	
 }
